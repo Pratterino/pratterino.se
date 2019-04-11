7610b69484fc992cdd521d2574d56102ca8e2294
Pär Strandberg <par.strandberg@gmail.com>
2019-04-11 23:35:04 +0200

    added game links

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,20 @@ import './App.scss';
 
 class App extends Component {
     skills = {
-        frontend: ["React", "Redux", "HTML", "CSS3", "SASS", "JSON", "Async/Await"],
-        backend: ["Java 8+", "Java spring", "NodeJS"],
+        frontend: ["React", "Redux", "HTML", "CSS3", "SASS", "Async/Await"],
+        backend: ["Java 8+", "Java Spring", "NodeJS"],
     };
+
+    games = [{
+        name: "Hitta Pärs händer!",
+        link: "/games/hands",
+    }, {
+        name: "Dwarven Conflicts",
+        link: "https://shieldwebdev.com/dwacon.html",
+    }, {
+        name: "Safaristaden",
+        link: "/games/safari",
+    }];
 
     renderSkill(skill) {
         return this.skills[skill].map(skill => (
@@ -22,6 +33,14 @@ class App extends Component {
                     {this.renderSkill("frontend")}
                     <br/>
                     {this.renderSkill("backend")}
+                </div>
+                <div className="games-container">
+                    <h4>Games</h4>
+                    {this.games.map(game => {
+                        return (
+                            <a className="game" href={game.link}>{game.name}</a>
+                        )
+                    })}
                 </div>
                 <footer>
                     <a href="mailto:par.strandberg@gmail.com">par.strandberg@gmail.com</a>
