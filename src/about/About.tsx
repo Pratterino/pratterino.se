390338f37c491cb0c0dfaac8c7fe6ce26d0af940
Pär Strandberg <par.strandberg@gmail.com>
2020-02-17 23:02:50 +0100

    simplifed about with more interesting information. removed brands section.

--- src/about/About.tsx
+++ src/about/About.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
-import {faBandcamp, faGithub, faInstagram, faLinkedin} from '@fortawesome/free-brands-svg-icons'
+import {faBandcamp, faGithub, faInstagram, faLinkedin, faYoutube} from '@fortawesome/free-brands-svg-icons'
 import './About.scss';
 
 function About() {
@@ -12,7 +12,24 @@ function About() {
             <section className="content">
                 <div>
                     <header>Pär Strandberg</header>
-                    <div><p>Jag är en utvecklare med främst inriktning mot frontend.</p>
+                    <ol>
+                        <li>Föddes i Umeå.</li>
+                        <li>Gick IT-Mediaprogrammet på John Bauergymnasiet i Umeå. (2006-2009)</li>
+                        <li>Startade upp och arbetade i Servicedisken på Dragonskolans gymnasium, städade på
+                            Norrlandsoperan.
+                        </li>
+                        <li>Flyttade till Växjö och gick Interaktiva digitala medier på Linnéuniversitetet. (2011-2014)
+                        </li>
+                        <li>Cyklade hem från Växjö till Umeå. (2012)</li>
+                        <li>Studerade en utbytestermin i Cork, Irland. (HT2013)</li>
+                        <li>Började på Cygni och deras talangprogram. (november 2014)</li>
+                        <li>Blev färdig med talangprogrammet. (2017)</li>
+                        <li>Jobbat på uppdrag som Cygni-konsult hos bland annat: Com hem, Eniro, Telia.</li>
+                        <li>Gillar att hålla på med improvisationsteater, brygga öl, och spela instrument.</li>
+                    </ol>
+                    <hr/>
+                    <div>
+                        <p>Jag är en utvecklare med främst inriktning mot frontend.</p>
                         <p>Kolla in <a
                             href="https://mobtimer.com/">mobtimer.com</a> för en timer som underlättar arbetet för
                             team att genomföra mobbprogrammering.</p>
@@ -24,16 +41,16 @@ function About() {
                         <a target="_blank"
                            rel="noopener noreferrer"
                            href="https://www.linkedin.com/in/p%C3%A4r-strandberg-271309185/"><FontAwesomeIcon
-                            icon={faLinkedin}/>LinkedIn</a> /
+                            icon={faLinkedin}/>LinkedIn</a>/
                         <a target="_blank" rel="noopener noreferrer"
                            href="https://instagram.com/pratterino"><FontAwesomeIcon
                             icon={faInstagram}/>Instagram</a>
                         <a target="_blank" rel="noopener noreferrer"
+                           href="https://youtube.com/tachiokuling/"><FontAwesomeIcon
+                            icon={faYoutube}/>YouTube</a>/
+                        <a target="_blank" rel="noopener noreferrer"
                            href="https://pratterino.bandcamp.com/"><FontAwesomeIcon
                             icon={faBandcamp}/>Bandcamp</a>
-                        <a target="_blank" rel="noopener noreferrer"
-                           href="https://soundcloud.com/pratterino/"><FontAwesomeIcon
-                            icon={faBandcamp}/>Soundcloud</a>
                     </footer>
                 </div>
             </section>
--- src/app/App.tsx
+++ src/app/App.tsx
@@ -12,7 +12,6 @@ interface ISkill {
 function App() {
     return [
         <About/>,
-        <Work/>,
     ];
 }
 
