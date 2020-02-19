3c7ea548c55d62427e5b1b516e4c64c7459d8f41
Pär Strandberg <par.strandberg@gmail.com>
2020-02-19 20:59:33 +0100

    addded dots and better responsiveness. (#3)

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,13 +1,6 @@
 import React from 'react';
 import About from '../about/About';
-import Work from "../work/Work";
-import Projects from "../projects/Projects";
 import './App.scss';
-
-interface ISkill {
-    frontend: Array<string>
-    backend: Array<string>
-}
 
 function App() {
     return [
